8e0a3b378ec857887dbfbda397d71be9d7a6f301
DovydasMar <mardovydas@gmail.com>
2023-12-05 13:45:43 +0200

    done cards

--- javascript.js
+++ javascript.js
@@ -23,4 +23,20 @@ function getAllCards(url) {
 }
 getAllCards(baseUrl);
 
-function createItem(postObj) {}
+function createItem(postObj) {
+  const card = document.createElement("div");
+  card.classList.add("col");
+  card.innerHTML = `
+  <div class="card h-100">
+    <img src="${postObj.image}" class="card-img-top" alt="https://picsum.photos/200/300">
+    <div class="card-body">
+      <h5 class="card-title"> ${postObj.city} </h5>
+      <p class="card-text">${postObj.description}</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">${postObj.price} europietiškų</small>
+    </div>
+  </div>
+`;
+  els.cardContainer.append(card);
+}
